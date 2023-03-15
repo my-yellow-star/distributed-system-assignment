bf3353e4d182325d64c83f5be9e55a57b669cf70
triphero-chan <c01034784796@gmail.com>
2023-03-15 10:35:53 +0900

    chore: init project

---
+++ src/main/kotlin/assignment/chana/distributedsystem/DistributedSystemApplication.kt
@@ -0,0 +1,11 @@
+package assignment.chana.distributedsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DistributedSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<DistributedSystemApplication>(*args)
+}
---
+++ src/test/kotlin/assignment/chana/distributedsystem/DistributedSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package assignment.chana.distributedsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DistributedSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
