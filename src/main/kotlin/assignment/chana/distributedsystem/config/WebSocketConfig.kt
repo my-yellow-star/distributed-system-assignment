2b99c863c1b221e87769f9bf9b07c3bac9d298e6
myYellowStar <c01034784796@gmail.com>
2023-04-02 02:18:35 +0900

    fix: 파일 송 수신 시 용량 한도 설정

--- src/main/kotlin/assignment/chana/distributedsystem/config/WebSocketConfig.kt
+++ src/main/kotlin/assignment/chana/distributedsystem/config/WebSocketConfig.kt
@@ -1,5 +1,6 @@
 package assignment.chana.distributedsystem.config
 
+import assignment.chana.distributedsystem.auth.UserRepository
 import assignment.chana.distributedsystem.file.FileSocketHandler
 import assignment.chana.distributedsystem.file.FileUploader
 import assignment.chana.distributedsystem.file.infra.LocalStorageFileUploader
@@ -9,12 +10,22 @@ import org.springframework.web.socket.WebSocketHandler
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean
+
 
 @Configuration
 @EnableWebSocket
 class WebSocketConfig : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(socketHandler(), "/upload")
+    }
+
+    @Bean
+    fun createWebSocketContainer(): ServletServerContainerFactoryBean? {
+        val container = ServletServerContainerFactoryBean()
+        container.setMaxTextMessageBufferSize(500000)
+        container.setMaxBinaryMessageBufferSize(500000)
+        return container
     }
 
     @Bean
